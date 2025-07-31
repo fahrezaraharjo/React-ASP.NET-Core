2cbcaaac79c21e2aa2202ff27a23763d68f407f5
fahrezaraharjo <fahrezaraharjo92@gmail.com>
2025-07-31 08:56:43 +0700

    first commit

--- ReactApp1.Server/obj/Debug/net8.0/ReactApp1.Server.AssemblyInfo.cs
+++ ReactApp1.Server/obj/Debug/net8.0/ReactApp1.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ReactApp1.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5af405a1e7a8193045614d4514b4110d631fc34")]
 [assembly: System.Reflection.AssemblyProductAttribute("ReactApp1.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ReactApp1.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
